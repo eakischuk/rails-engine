9a2e9e24da410f7b6ac8216fc1ec4fd4b83df47b
Erika Kischuk <erika.kischuk@gmail.com>
2021-10-22 15:23:09 -0600

    Adjust factory names, remove mock

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :mock_item do
+  factory :item do
     name { Faker::Commerce.product_name }
     description { Faker::Lorem.sentence }
     unit_price { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :mock_merchant do
+  factory :merchant do
     name { Faker::Company.name }
   end
 end
