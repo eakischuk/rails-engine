b259d2d22458251dfadffcf02fd1e2b6a23d695a
Erika Kischuk <erika.kischuk@gmail.com>
2021-10-25 17:42:37 -0600

    Add rescue from redirect for not found error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,20 @@ class ApplicationController < ActionController::API
   def record_not_found
     render plain: "404 Not Found", status: 404
   end
+
+  def find_page
+    if params[:page].to_i == 0
+      params[:page].to_i
+    else
+      params[:page].to_i - 1
+    end
+  end
+
+  def find_per_page
+    if params[:per_page].present?
+      params[:per_page].to_i
+    else
+      20
+    end
+  end
 end
