6334e790117099ebf17fff286beff1cdd800f611
Erika Kischuk <erika.kischuk@gmail.com>
2021-10-25 20:32:32 -0600

    Add after action for create and bad request error for record invalid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :bad_request
+  after_action :set_code_on_create, only: [:create]
 
   private
 
   def record_not_found
     render plain: "404 Not Found", status: 404
+  end
+
+  def bad_request
+    render plain: "400 Bad Request", status: 400
+  end
+
+  def set_code_on_create
+    response.status = 201 if response.status == 200
   end
 
   def find_page
