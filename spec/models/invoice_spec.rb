e5b74f712dbe7ee9f604f9cf1d5280c0177391d6
Erika Kischuk <erika.kischuk@gmail.com>
2021-10-26 14:55:33 -0600

    Add test for destroy empty invoices class method

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -5,4 +5,24 @@ RSpec.describe Invoice, type: :model do
     it { should have_many(:invoice_items) }
     it { should have_many(:transactions) }
   end
+
+  describe 'class methods' do
+    it 'destroy invoices with no items' do
+      item_1 = create(:item)
+      item_2 = create(:item)
+      invoice_1 = create(:invoice)
+      invoice_2 = create(:invoice)
+      invoice_item_1 = create(:invoice_item, invoice: invoice_1, item: item_1)
+      invoice_item_2 = create(:invoice_item, invoice: invoice_2, item: item_1)
+      invoice_item_3 = create(:invoice_item, invoice: invoice_2, item: item_2)
+
+      expect(Invoice.all.count).to eq(2)
+      item_1.destroy
+      Invoice.destroy_empty_invoices
+      expect(Invoice.all.count).to eq(1)
+      item_2.destroy
+      Invoice.destroy_empty_invoices
+      expect(Invoice.all.count).to eq(0)
+    end
+  end
 end
