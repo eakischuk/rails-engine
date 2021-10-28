13ba2d90c14ec302f9e83e4ab7cde5cf8cae443e
Erika Kischuk <erika.kischuk@gmail.com>
2021-10-28 15:49:13 -0600

    Add format for merchant revenue endpoints return values

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -25,4 +25,40 @@ class MerchantSerializer
     {  data: { }
     }
   end
+
+  def self.format_merchants_revenue(merchants)
+    {  data:
+      merchants.map do |merch|
+        {id: merch.id.to_s,
+        type: 'merchant_name_revenue',
+        attributes: {
+          name: merch.name,
+          revenue: merch.revenue
+          }}
+      end
+    }
+  end
+
+  def self.format_merchant_revenue(merchant)
+    {  data:
+        {id: merchant.id.to_s,
+        type: 'merchant_revenue',
+        attributes: {
+          revenue: merchant.total_revenue
+          }}
+    }
+  end
+
+  def self.format_merchants_items_sold(merchants)
+    {  data:
+      merchants.map do |merch|
+        {id: merch.id.to_s,
+        type: 'merchant_name_revenue',
+        attributes: {
+          name: merch.name,
+          count: merch.items_sold
+          }}
+      end
+    }
+  end
 end
