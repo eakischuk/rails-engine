47eceba95b3d9b97e11a20815710621a72a317b3
Erika Kischuk <erika.kischuk@gmail.com>
2021-10-27 21:31:49 -0600

    Add total_revenue_between class method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -8,4 +8,11 @@ class Invoice < ApplicationRecord
     empty_invoices = left_outer_joins(:invoice_items).where(invoice_items: {id: nil})
     empty_invoices.destroy_all
   end
+
+  def self.total_revenue_between(start_date, end_date)
+    joins(:invoice_items, :transactions)
+    .where('transactions.result = ?', "success")
+    .where('invoices.created_at >= ? AND invoices.created_at <= ?', start_date, end_date)
+    .sum('invoice_items.quantity * invoice_items.unit_price')
+  end
 end
