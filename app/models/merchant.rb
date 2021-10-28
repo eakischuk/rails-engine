55b5f364dfe6dc216c6a0ff91bbb07ea215471ca
Erika Kischuk <erika.kischuk@gmail.com>
2021-10-27 20:40:04 -0600

    Add class methods by_revenue and by_items_sold and instance method total_revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,28 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
   has_many :customers, through: :invoices
+
+  def self.by_revenue(limit)
+    joins(items: {invoice_items: {invoice: :transactions}})
+    .where('transactions.result = ?', "success")
+    .select("merchants.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue")
+    .group(:id)
+    .order(revenue: :desc)
+    .limit(limit)
+  end
+
+  def self.by_items_sold(limit)
+    joins(items: {invoice_items: {invoice: :transactions}})
+    .where('transactions.result = ?', "success")
+    .select("merchants.*, SUM(invoice_items.quantity) AS items_sold")
+    .group(:id)
+    .order(items_sold: :desc)
+    .limit(limit)
+  end
+
+  def total_revenue
+    invoice_items.joins(invoice: :transactions)
+    .where('transactions.result = ?', "success")
+    .sum('invoice_items.unit_price * invoice_items.quantity')
+  end
 end
