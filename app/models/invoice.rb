36bf6017f7ffca17b58380286186233d110e6a8b
Erika Kischuk <erika.kischuk@gmail.com>
2021-10-28 15:42:35 -0600

    Add date formatting methods

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -9,10 +9,22 @@ class Invoice < ApplicationRecord
     empty_invoices.destroy_all
   end
 
+  def self.start_date_format(start)
+    DateTime.strptime(start, '%Y-%m-%d').beginning_of_day
+  end
+
+  def self.end_date_format(end_date)
+    DateTime.strptime(end_date, '%Y-%m-%d').end_of_day
+  end
+
   def self.total_revenue_between(start_date, end_date)
+    start = start_date_format(start_date)
+    end_of_date = end_date_format(end_date)
     joins(:invoice_items, :transactions)
     .where('transactions.result = ?', "success")
-    .where('invoices.created_at >= ? AND invoices.created_at <= ?', start_date, end_date)
+    .where('invoices.created_at >= ? AND invoices.created_at <= ?', start, end_of_date)
+    .where('invoices.status = ?', 'shipped')
     .sum('invoice_items.quantity * invoice_items.unit_price')
   end
+
 end
